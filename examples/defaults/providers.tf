4eceb6f29e86ac381fbd2989e8dcc8431220f460
Jesse Wellenberg <jwellenberg@xpirit.com>
2024-12-06 15:45:26 +0100

    Compatibility version

--- examples/defaults/providers.tf
+++ examples/defaults/providers.tf
@@ -4,7 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.116.0"
+      version = "~> 3.116.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "~> 2.52.0"
     }
   }
 }
