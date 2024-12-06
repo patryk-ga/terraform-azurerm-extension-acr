12527770f312b121ed7d3730df69b98c91ba778b
Jesse Wellenberg <jwellenberg@xpirit.com>
2024-12-06 15:27:20 +0100

    Rename terraform, versions

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -3,13 +3,12 @@ module "defaults" {
    source = "../../src" 
 
    # Spoke group ids
-   service_principal_object_id = ""
-   owner_group_id = ""
-   contributor_group_id = ""
-   reader_group_id = ""
+   service_principal_object_id = "1cb8e956-959b-44fe-b293-d34d555701f3"
+   owner_group_id = "b55d99c9-4131-4409-b8cc-647a1e763dae"
+   contributor_group_id = "b55d99c9-4131-4409-b8cc-647a1e763dae"
+   reader_group_id = "b55d99c9-4131-4409-b8cc-647a1e763dae"
 
    # Acr access config
-   acr_resource_id = ""
-   acr_group_name = ""
-   acr_group_administrative_units = []
+   acr_resource_id = "/subscriptions/cbafd01c-a328-4557-a994-f3179498af36/resourceGroups/rg-joey-166/providers/Microsoft.ContainerRegistry/registries/acrjoey166"
+   acr_group_name = "demospoke-acrpull"
 }
